584b8dadf70e01789419aa1121b4842bb75943a2
ikkun2501 <ikkun2501@gmail.com>
2017-08-08 10:49:53 +0900

    暫定的修正
    gradleで実行するとプロパティが読み込まれなかった。
    現状はトークンをソースコード状に直接設定するように修正
    実行時はトークンを設定する必要がある。

--- SilentBeaProgrammingForSlack/src/main/java/com/bunji/Example00.java
+++ SilentBeaProgrammingForSlack/src/main/java/com/bunji/Example00.java
@@ -1,7 +1,6 @@
 package com.bunji;
 
 import java.io.IOException;
-import java.util.ResourceBundle;
 
 import org.riversun.slacklet.Slacklet;
 import org.riversun.slacklet.SlackletRequest;
@@ -11,46 +10,61 @@ import org.riversun.xternal.simpleslackapi.SlackChannel;
 
 public class Example00 {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        String botToken = ResourceBundle.getBundle("credentials").getString("slack.bot_api_token");
+		// String botToken =
+		// ResourceBundle.getBundle("credentials").getString("slack.bot_api_token");
+		String botToken = "";
 
-        SlackletService slackService = new SlackletService(botToken);
+		SlackletService slackService = new SlackletService(botToken);
 
-        slackService.addSlacklet(new Slacklet() {
+		slackService.addSlacklet(new Slacklet() {
 
-            @Override
-            public void onMessagePosted(SlackletRequest req, SlackletResponse resp) {
-                // メッセージがユーザーからポストされた
+			@Override
+			public void onMessagePosted(SlackletRequest req, SlackletResponse resp) {
+				// メッセージがユーザーからポストされた
 
-                // メッセージがポストされたチャンネルを取得する
-                SlackChannel channel = req.getChannel();
+				// メッセージがポストされたチャンネルを取得する
+				SlackChannel channel = req.getChannel();
 
-                if("bear-programming".equals(channel.getName())) {
+				if ("bear-programming".equals(channel.getName())) {
 
-                    // メッセージ本文を取得
-                    String content = req.getContent();
-                    if(content.contains("相談")) {
-                        resp.reply("ぼっとじゃなく同僚に相談したら？");
-                    } else if(content.contains("解決")) {
-                        resp.reply("俺の手柄だね");
-                    } else if(content.contains("?")) {
-                        resp.reply("相談する前に自分で調べなよ");
-                    }else if(content.contains("くまさん")) {
-                        resp.reply("呼んだ？");
-                    }  else{
-                        resp.reply("そうなんだ。");
-                    }
+					// メッセージ本文を取得
+					String content = req.getContent();
+					if (content.contains("相談")) {
+						resp.reply("ぼっとじゃなく同僚に相談したら？");
+					} else if (content.contains("解決")) {
+						resp.reply("俺の手柄だね");
+					} else if (content.contains("?")) {
+						resp.reply("相談する前に自分で調べなよ");
+					} else if (content.contains("くまさん")) {
+						resp.reply("呼んだ？");
+					} else {
+						resp.reply("そうなんだ。");
+					}
 
-                    // メッセージがポストされたチャンネルに対して、BOTからメッセージを送る
+					// メッセージがポストされたチャンネルに対して、BOTからメッセージを送る
 
-                }
-            }
-        });
+				}
 
-        // slackletserviceを開始(slackに接続)
-        slackService.start();
+				if ("general".equals(channel.getName())) {
 
-    }
+					// メッセージ本文を取得
+					String content = req.getContent();
+
+					if (content.contains("くまさん")) {
+						resp.reply("呼んだ？");
+					}
+
+					// メッセージがポストされたチャンネルに対して、BOTからメッセージを送る
+
+				}
+			}
+		});
+
+		// slackletserviceを開始(slackに接続)
+		slackService.start();
+
+	}
 
 }
